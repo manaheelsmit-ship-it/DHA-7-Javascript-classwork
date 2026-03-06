90bd7be490a25b3913bc94369a0399570888ebf7
manaheel jamil <manaheelsmit@gmail.com>
2026-03-06 12:16:36 +0500

    javascript first class

---
+++ script.js
@@ -0,0 +1,9 @@
+// alert('hello javascript')
+
+var userName = 'admin'
+userName = 'john'
+userName = 'rizwan'
+console.log(userName) // we will use this for printing value in console
+
+var fatherName;  //declaration
+fatherName = 'saleem' //initialization 
