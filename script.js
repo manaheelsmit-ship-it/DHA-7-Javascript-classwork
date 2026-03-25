2a481200567bec45b9ed1a76ce8e4a045d29cbfd
manaheel jamil <manaheelsmit@gmail.com>
2026-03-25 12:38:21 +0500

    chapter 6,9,10

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 // alert('hello javascript')
 
-var userName = 'admin'
-userName = 'john'
-userName = 'rizwan'
-console.log(userName) // we will use this for printing value in console
+// var userName = 'admin'
+// userName = 'john'
+// userName = 'rizwan'
+// console.log(userName) // we will use this for printing value in console
+// alert('hello '+userName)
+// var fatherName;  //declaration
+// fatherName = 'saleem' //initialization
+// var num1 = 4
+// var num2 = 5
+// console.log(num1 + num2 - 76)
+// var userName = prompt('what is your name ?')
+// alert('hello '+ userName)
+// var value1 = Number(prompt('enter your first value'))
+// var value2 = Number(prompt('enter your 2nd value'))
+// console.log(typeof value1)
+// console.log(typeof value2)
+// console.log(value1+value2)
+// var num1 = 7
+// var sum = num1++ + ++num1 + --num1
+// console.log(sum)
 
-var fatherName;  //declaration
-fatherName = 'saleem' //initialization 
+// var userName = prompt('how old are you??') 
+// var num = 3
+// if ( num != 9) {
+//     console.log('got it')
+// } else {
+//     console.log('enter your value ')
+// }
+//  =  //assigning a variable
+//  == //comparision
+//  ===  //comparision
+//  != 
+//  !==
+//  >
+//  <
+//  >=
+//  =>
